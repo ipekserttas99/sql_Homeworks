d1661743faa42dc04b89c1bc9611bf5cbde6730d
İpek Serttaş <82452799+ipekserttas99@users.noreply.github.com>
2021-12-07 22:57:54 +0300

    Update sqlodev1.sql

--- sqlodev1.sql
+++ sqlodev1.sql
@@ -1,6 +1,11 @@
+--1.soru
 SELECT title,description FROM film;
+--2.soru
 SELECT * FROM film WHERE length>60 AND length<75;
+--3.soru
 SELECT * FROM film WHERE rental_rate =0.99 AND (replacement_cost = 12.99 OR replacement_cost =28.99);
+--4.soru
 SELECT last_name FROM customer WHERE first_name = 'Mary'; 
 --SMITH
+--5.soru
 SELECT * FROM film WHERE NOT length > 50 AND NOT(rental_rate=2.99 OR rental_rate=4.99); 
