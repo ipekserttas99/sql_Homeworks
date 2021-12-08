9a16c34512d95a657a99f634b9d483cfc64c9821
İpek Serttaş <82452799+ipekserttas99@users.noreply.github.com>
2021-12-08 14:33:09 +0300

    Add files via upload

---
+++ sqlodev5.sql
@@ -0,0 +1,6 @@
+--1.SORU
+SELECT * FROM film WHERE title LIKE '%n' ORDER BY length DESC LIMIT 5;
+--2.SORU
+SELECT * FROM film WHERE title LIKE '%n' ORDER BY length OFFSET 5 LIMIT 5;
+--3.SORU
+SELECT * FROM customer WHERE store_id=1 ORDER BY last_name DESC LIMIT 4;
