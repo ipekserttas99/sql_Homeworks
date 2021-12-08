c2fc4cfbcdbd3885969019f3ca2cc77039fbb5b1
İpek Serttaş <82452799+ipekserttas99@users.noreply.github.com>
2021-12-08 14:51:36 +0300

    Update sqlodev4.sql

--- sqlodev4.sql
+++ sqlodev4.sql
@@ -1,10 +1,14 @@
---1.SORU
+--film tablosunda bulunan replacement_cost sütununda bulunan birbirinden farklı değerleri sıralayınız.
 SELECT DISTINCT replacement_cost FROM film;
---2.SORU
+
+--film tablosunda bulunan replacement_cost sütununda birbirinden farklı kaç tane veri vardır?
 SELECT COUNT(DISTINCT replacement_cost) FROM film;
---3.SORU
+
+--film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
 SELECT COUNT(title) FROM film WHERE title LIKE 'T%' AND rating='G';
---4.SORU
+
+--country tablosunda bulunan ülke isimlerinden (country) kaç tanesi 5 karakterden oluşmaktadır?
 SELECT COUNT(country) FROM country WHERE country LIKE '____%';
---5.SORU
+
+--city tablosundaki şehir isimlerinin kaç tanesi 'R' veya r karakteri ile biter?
 SELECT COUNT(city) FROM city WHERE city LIKE '%r';
