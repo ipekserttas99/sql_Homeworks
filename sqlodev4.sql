6f68f84b1b74a666ceca2d8d3383add01f87aa58
İpek Serttaş <82452799+ipekserttas99@users.noreply.github.com>
2021-12-08 13:37:12 +0300

    Add files via upload

---
+++ sqlodev4.sql
@@ -0,0 +1,10 @@
+--1.SORU
+SELECT DISTINCT replacement_cost FROM film;
+--2.SORU
+SELECT DISTINCT replacement_cost FROM film;
+--3.SORU
+SELECT COUNT(title) FROM film WHERE title LIKE 'T%' AND rating='G';
+--4.SORU
+SELECT COUNT(country) FROM country WHERE country LIKE '____%';
+--5.SORU
+SELECT COUNT(city) FROM city WHERE city LIKE '%r';
